8d5680438962a7456dddae267d7ef0093b199d04
maix0 <maieul.dev@familleboyer.net>
2025-02-14 22:23:55 +0100

    actually catch the errors on the main.cpp

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 15:18:37 by maiboyer          #+#    #+#             */
-/*   Updated: 2025/02/14 20:50:47 by maiboyer         ###   ########.fr       */
+/*   Updated: 2025/02/14 22:22:39 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,21 +28,24 @@ double get_price_at(const Date& date, const BitcoinPrices& prices) {
 
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
-		std::cerr << "usage: " << (argv[0] ? argv[0] : "./bitcoin") << " <filename>" << std::endl;
+		std::cerr << "usage: " << (argv[0] ? argv[0] : "./btc") << " <filename>" << std::endl;
 		return 1;
 	}
-	BitcoinPrices				 prices	  = BitcoinPrices::from_file("./data.csv");
-	BitcoinHoldings				 holdings = BitcoinHoldings::from_file(argv[1]);
+	try {
+		BitcoinPrices				 prices	  = BitcoinPrices::from_file("./data.csv");
+		BitcoinHoldings				 holdings = BitcoinHoldings::from_file(argv[1]);
+		const BitcoinHoldings::Data& data	  = holdings.getData();
 
-	const BitcoinHoldings::Data& data	  = holdings.getData();
-
-	for (BitcoinHoldings::Data::const_iterator i = data.begin(); i != data.end(); i++) {
-		double price = get_price_at(i->first, prices);
-		if (price < 0.f) {
-			std::cout << "Couldn't get price for " << i->second << " at " << i->first << " since it is before the first recorded price !"
-					  << std::endl;
-			continue;
+		for (BitcoinHoldings::Data::const_iterator i = data.begin(); i != data.end(); i++) {
+			double price = get_price_at(i->first, prices);
+			if (price < 0.f) {
+				std::cout << "Couldn't get price for " << i->second << " at " << i->first
+						  << " since it is before the first recorded price !" << std::endl;
+				continue;
+			}
+			std::cout << i->first << " = " << i->second * price << std::endl;
 		}
-		std::cout << i->first << " = " << i->second * price << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
 	}
 }
