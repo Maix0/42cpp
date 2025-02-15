73f59553b5c3035d985ff35078e68a16141ced87
maix0 <maieul.dev@familleboyer.net>
2025-02-15 20:39:08 +0100

    update: fixed easyfind to show index

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 15:19:01 by maiboyer          #+#    #+#             */
-/*   Updated: 2025/02/15 18:24:23 by maiboyer         ###   ########.fr       */
+/*   Updated: 2025/02/15 20:38:30 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 #define EASYFIND_HPP
 
 #include <algorithm>
-template <typename T> bool easyfind(const T &c, int v) {
-  typename T::const_iterator val = std::find(c.begin(), c.end(), v);
+#include <iostream>
 
-  return (val != c.end());
+template <typename T>
+bool easyfind(const T& c, int v) {
+	typename T::const_iterator it = std::find(c.begin(), c.end(), v);
+	if (it != c.end()) {
+		std::cout << "Found " << v << " in the container at index " << std::distance(c.begin(), it) << std::endl;
+		return (true);
+	} else {
+		std::cout << "Couldn't find " << v << " in the container" << std::endl;
+		return (false);
+	}
 }
 
 #endif /* EASYFIND_HPP */
