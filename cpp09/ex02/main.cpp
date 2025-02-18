973b575f729a371ad95227873a11cfe65a974aed
maix0 <maieul.dev@familleboyer.net>
2025-02-18 23:10:43 +0100

    update: better main.cpp

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,11 +6,13 @@
 /*   By: maiboyer <maiboyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 16:49:00 by maiboyer          #+#    #+#             */
-/*   Updated: 2025/02/18 22:16:22 by maiboyer         ###   ########.fr       */
+/*   Updated: 2025/02/18 23:09:44 by maiboyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <list>
 #include <vector>
@@ -26,38 +28,69 @@ bool is_sorted(Iterator begin, Iterator end) {
 	return true;
 }
 
+#define START_TIMER before = std::clock();
+#define END_TIMER(MSG)    \
+	after = std::clock(); \
+	std::cout << MSG << " took " << ((double)(after - before) * 1000000.0) / (double)(CLOCKS_PER_SEC) << "us to do !" << std::endl;
+
+template <typename C>
+void print_container(const C& c, const std::string& type, const std::string& name, int max = 50) {
+	int count = 0;
+	std::cout << type << " " << name << " = [";
+	typename C::const_iterator it = c.begin();
+	if (it != c.end()) {
+		std::cout << *it;
+		count++;
+	}
+	for (; it != c.end(); it++) {
+		std::cout << ", " << *it;
+		if (++count >= max) {
+			std::cout << ", ... ";
+			break;
+		}
+	}
+	std::cout << "]" << std::endl << std::endl;
+}
+
+template <typename C>
+void do_test(int values[], size_t size, std::string name) {
+	C			 unsorted;
+
+	// such that the variable are declared now :)
+	std::clock_t before;
+	std::clock_t after;
+
+	START_TIMER;
+	for (unsigned int i = 0; i < size; i++)
+		unsorted.push_back(values[i]);
+	END_TIMER("pushing " << size << " elements into " << name);
+
+	print_container(unsorted, "unsorted", name);
+
+	START_TIMER;
+	C sorted = PMergeMe(unsorted);
+	END_TIMER("sorting " << size << " elements using " << name);
+
+	print_container(sorted, "sorted", name);
+
+	if (is_sorted<typename C::iterator>(sorted.begin(), sorted.end()))
+		std::cout << name << " is indeed sorted !" << std::endl;
+	else
+		std::cout << name << " is NOT sorted !" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+}
+
+#define NB 3000
+
 int main() {
-	int values[] = {
-		33, 63, 16, 66, 32, 69, 87, 81, 11, 94, 82, 50, 79, 27, 10, 90, 23, 77, 97, 58, 40, 36, 39, 7,	20,
-		98, 14, 17, 62, 64, 91, 18, 35, 96, 10, 86, 47, 68, 1,	60, 38, 76, 12, 61, 72, 31, 49, 8,	75, 34,
-		3,	19, 5,	85, 45, 95, 28, 41, 42, 70, 25, 99, 93, 83, 37, 30, 51, 84, 2,	74, 4,	44, 15, 43, 67,
-		24, 55, 80, 52, 57, 9,	21, 92, 53, 73, 22, 89, 71, 46, 26, 88, 13, 65, 54, 6,	48, 29, 56, 78, 59,
-	};
+	int values[NB] = {0};
 
-	std::vector<int> v;
-	for (unsigned int i = 0; i < sizeof(values) / sizeof(*values); i++)
-		v.push_back(values[i]);
+	std::srand(std::time(NULL));
+	for (int i = 0; i < NB; i++)
+		values[i] = std::rand() % (3 * NB);
 
-	v = PMergeMe<std::vector>(v);
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	if (is_sorted<std::vector<int>::iterator>(v.begin(), v.end()))
-		std::cout << "it is indeed sorted !" << std::endl;
-	else
-		std::cout << "it is NOT sorted !" << std::endl;
-
-	std::list<int> l;
-	for (unsigned int i = 0; i < sizeof(values) / sizeof(*values); i++)
-		l.push_back(values[i]);
-
-	l = PMergeMe<std::list>(l);
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-	if (is_sorted<std::list<int>::iterator>(l.begin(), l.end()))
-		std::cout << "it is indeed sorted !" << std::endl;
-	else
-		std::cout << "it is NOT sorted !" << std::endl;
+	do_test<std::vector<int> /**/>(values, sizeof(values) / sizeof(values[0]), "std::vector");
+	do_test<std::list<int> /**/>(values, sizeof(values) / sizeof(values[0]), "std::list");
 }
